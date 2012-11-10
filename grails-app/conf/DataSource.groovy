88762bda6170a9ff78eaaa85d8456ff7f2e025e3
“shaurav” <shaurav@intelligrape.com>
2012-11-10 14:18:18 +0530

    removed uncommented code of bootstrap

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,7 +1,7 @@
 grails {
     mongo {
-        host = "localhost"
-        port = 27017
+//        host = "localhost"
+//        port = 27017
 //        username = "blah"
 //        password = "blah"
         databaseName = "recommender-api"
