2214f0f28f18c4e0c6dfeefbd635a8b0d25bd987
Kushal Likhi <kushal@intelligrape.com>
2012-11-11 20:38:45 +0530

    Merge branch 'master' of github.com:kushal-likhi/recommender-api
    Conflicts:
    	grails-app/conf/BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,27 +8,26 @@ class BootStrap {
     def init = { servletContext ->
 
         //To Create And Drop the database
-        switch (GrailsUtil.environment) {
+        /*  switch (GrailsUtil.environment) {
             case GrailsApplication.ENV_DEVELOPMENT:
             case GrailsApplication.ENV_TEST:
             case 'qa':
                 Mongo m = new Mongo()
-//                m.dropDatabase("recommender-api")
+                m.dropDatabase("recommender-api")
                 break;
             case GrailsApplication.ENV_PRODUCTION:
                 break;
+        }*/
+
+        if (!User.collection.findOne([email: "kushal@intelligrape.com"])) {
+            new User(
+                    email: "kushal@intelligrape.com",
+                    enabled: true,
+                    firstName: "Kushal",
+                    lastName: "Likhi",
+                    password: "yo".encodeAsSHA256()
+            ).save()
         }
-
-//        new User(
-//                email: "kushal@intelligrape.com",
-//                enabled: true,
-//                firstName: "Kushal",
-//                lastName: "Likhi",
-//                password: "yo".encodeAsSHA256(),
-//                isAdmin: true
-//        ).save()
-
-    }
 
     def destroy = {
     }
